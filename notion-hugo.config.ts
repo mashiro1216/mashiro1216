79570be656a95a92be38395fd18230f450e7fb87
mashiro1216 <99098120+mashiro1216@users.noreply.github.com>
2024-09-03 03:45:55 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://mashiro1216.notion.site/Notion-DoIt-e420ebefe79444e495202667c0bcdfe1',
         pages: [
             // {
             //     page_id: '<page_id>',
